3f31fe8dbbb8ba39c8a149f0598d2aecb9b11e6f
Kyle Harding <kharding@marchnetworks.com>
2019-06-25 13:02:27 -0400

    rough in support for a dns-cloudflare container to get and renew certs

---
+++ dns-cloudflare/renew.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -ex
+
+# only continue if the required vars are set
+[ -n "${CLOUDFLARE_EMAIL}" ] || exit 1
+[ -n "${CLOUDFLARE_API_KEY}" ] || exit 1
+[ -n "${CLOUDFLARE_DOMAINS}" ] || exit 1
+
+# write a secure cloudflare credentials file
+# https://certbot-dns-cloudflare.readthedocs.io/en/stable/#
+cat > /root/.secrets/cloudflare.ini << EOF
+dns_cloudflare_email = ${CLOUDFLARE_EMAIL}
+dns_cloudflare_api_key = ${CLOUDFLARE_API_KEY}
+EOF
+
+# set permissions secrets
+chmod 0700 /root/.secrets
+chmod 0400 /root/.secrets/cloudflare.ini
+
+while :
+do
+    # get certs for adguard encryption
+    # https://github.com/AdguardTeam/AdGuardHome/wiki/Encryption
+    certbot certonly --dns-cloudflare -n \
+        --dns-cloudflare-credentials /root/.secrets/cloudflare.ini \
+        -m "${CLOUDFLARE_EMAIL}" --agree-tos \
+        -d "${CLOUDFLARE_DOMAINS}"
+
+
+    # wait 24-hours before renewing
+    sleep 86400
+done
